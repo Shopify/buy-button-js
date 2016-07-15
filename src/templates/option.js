95e8ae326ec974937fe0249b4435a717dbed93b7
Tessa Thornton <tessathornton@gmail.com>
2016-07-15 13:43:46 -0400

    quotation marks?

--- src/templates/option.js
+++ src/templates/option.js
@@ -2,7 +2,7 @@ const optionTemplates = {
       option: '<div class={{data.classes.option}}>' +
               '<label>{{data.name}}</label>' +
               '<div class="shopify-select">' +
-                '<select class={{data.classes.select}} name={{data.name}}>' +
+                '<select class="{{data.classes.select}}" name="{{data.name}}">' +
                   '{{#data.values}}' +
                     '<option {{#disabled}}disabled{{/disabled}} {{#selected}}selected{{/selected}} value={{name}}>{{name}}</option>' +
                   '{{/data.values}}' +
