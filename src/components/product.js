efd18d35c1f04f1ec9e51a85b9584dcf3874d133
Tessa Thornton <tessathornton@gmail.com>
2016-07-28 16:31:34 -0400

    fix broken quantity button

--- src/components/product.js
+++ src/components/product.js
@@ -62,9 +62,9 @@ export default class Product extends Component {
       click: this.closeCartOnBgClick.bind(this),
       [`change .${this.classes.option.select}`]: this.onOptionSelect.bind(this),
       [`click .${this.classes.product.button}`]: this.onButtonClick.bind(this),
-      [`click .${this.classes.quantityButton}.quantity-increment`]: this.onQuantityIncrement.bind(this, 1),
-      [`click .${this.classes.quantityButton}.quantity-decrement`]: this.onQuantityIncrement.bind(this, -1),
-      [`focusout .${this.classes.quantityInput}`]: this.onQuantityBlur.bind(this),
+      [`click .${this.classes.product.quantityButton}.quantity-increment`]: this.onQuantityIncrement.bind(this, 1),
+      [`click .${this.classes.product.quantityButton}.quantity-decrement`]: this.onQuantityIncrement.bind(this, -1),
+      [`focusout .${this.classes.product.quantityInput}`]: this.onQuantityBlur.bind(this),
     });
   }
 
