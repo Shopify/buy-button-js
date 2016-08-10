5a46f8f0631d4403240f9a996a4353ba18969e1a
Tim Anema <tim.anema@shopify.com>
2016-08-10 15:47:01 -0400

    initial start on adapter

---
+++ src/legacy/default-options.js
@@ -0,0 +1,25 @@
+const defaultOptions = {
+  product: {
+    text: {},
+    contents: {},
+    styles: {
+      button: {},
+      variantTitle: {},
+      title: {},
+      price: {},
+    },
+  },
+  productSet: {
+    styles: {
+      product: {},
+    },
+    products: {},
+  },
+  modalProduct: {
+    styles: {
+      button: {},
+    },
+  },
+};
+
+export default defaultOptions;
