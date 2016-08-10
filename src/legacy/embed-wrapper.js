785cf6cd358efdc810265472d290c7548451eb06
Tim Anema <tim.anema@shopify.com>
2016-08-10 16:33:20 -0400

    small changes in code style

--- src/legacy/embed-wrapper.js
+++ src/legacy/embed-wrapper.js
@@ -4,14 +4,10 @@ import OptionsTransform from './options-transform';
 class EmbedWrapper {
   constructor(element) {
     this.element = element;
-    this.options = {};
-    this.generateEmbedOptions();
-  }
-  generateEmbedOptions() {
-    const options = {};
-    attributes.forEach((attr) => {
-      options[attr] = this.element.getAttribute(`data-${attr}`);
-    });
+    const options = attributes.reduce((opts, attr) => {
+      opts[attr] = this.element.getAttribute(`data-${attr}`);
+      return opts;
+    }, {});
     this.embedType = options.embed_type;
     this.handle = options[`${this.embed_type}_handle`];
     this.options = new OptionsTransform(this.embedType, options);
