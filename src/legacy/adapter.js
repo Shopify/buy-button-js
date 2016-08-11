454ab7ea5c57b83a61f9b8911a1876f732f753a5
Tim Anema <tim.anema@shopify.com>
2016-08-11 09:41:56 -0400

    fixed the transforms output

--- src/legacy/adapter.js
+++ src/legacy/adapter.js
@@ -7,18 +7,20 @@ class Adapter {
     this.uis = {};
     this.elements = [...document.querySelectorAll('[data-embed_type]')].map((element) => {
       const elem = new EmbedWrapper(element);
-      elem.render(this.getUI(elem));
+      elem.render(this.getUI(elem.shop));
       return elem;
     });
   }
-  getUI(elem) {
-    this.clients[elem.shop] = this.clients[elem.shop] || ShopifyBuy.buildClient({
-      apiKey: '395ba487a5981e6e573b5ab104645271',
-      appId: 6,
-      domain: elem.shop,
-    });
-    this.uis[elem.shop] = this.uis[elem.shop] || ShopifyBuy.UI.init(this.clients[elem.shop]);
-    return this.uis[elem.shop];
+  getUI(shop) {
+    if (!this.uis[shop]) {
+      this.clients[shop] = this.clients[shop] || ShopifyBuy.buildClient({
+        apiKey: '395ba487a5981e6e573b5ab104645271',
+        appId: 6,
+        domain: shop,
+      });
+      this.uis[shop] = this.uis[shop] || ShopifyBuy.UI.init(this.clients[shop]);
+    }
+    return this.uis[shop];
   }
 }
 
