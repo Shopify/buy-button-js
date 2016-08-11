8ec1c3e65305fb63413573f7f86b44cc38c63a70
Tim Anema <tim.anema@shopify.com>
2016-08-11 11:30:30 -0400

    trying to get all components working even with multiple script tags

--- src/legacy/adapter.js
+++ src/legacy/adapter.js
@@ -1,27 +1,32 @@
 import ShopifyBuy from '../shopify-buy-ui';
 import EmbedWrapper from './embed-wrapper';
 
+const apiKey = window.SHOPIFY_BUY_UI_ADAPTER_API_KEY || '395ba487a5981e6e573b5ab104645271';
+const appId = window.SHOPIFY_BUY_UI_ADAPTER_APP_ID || 6;
+
 class Adapter {
   constructor() {
     this.clients = {};
     this.uis = {};
+  }
+  init() {
     this.elements = [...document.querySelectorAll('[data-embed_type]')].map((element) => {
       const elem = new EmbedWrapper(element);
-      elem.render(this.getUI(elem.shop));
+      elem.render(this.getShopUI(elem.shop));
       return elem;
     });
   }
-  getUI(shop) {
+  getShopUI(shop) {
     if (!this.uis[shop]) {
-      this.clients[shop] = this.clients[shop] || ShopifyBuy.buildClient({
-        apiKey: '395ba487a5981e6e573b5ab104645271',
-        appId: 6,
-        domain: shop,
-      });
-      this.uis[shop] = this.uis[shop] || ShopifyBuy.UI.init(this.clients[shop]);
+      this.clients[shop] = ShopifyBuy.buildClient({apiKey, appId, domain: shop});
+      this.uis[shop] = ShopifyBuy.UI.init(this.clients[shop]);
     }
     return this.uis[shop];
   }
 }
 
-export default new Adapter();
+const adapter = new Adapter();
+
+(() => { setTimeout(adapter.init.bind(adapter)); })();
+
+export default adapter;
