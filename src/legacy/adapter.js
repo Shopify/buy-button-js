bced0e336c37c65c76712fd9cddd92158e517f3c
Tim Anema <tim.anema@shopify.com>
2016-08-17 11:15:50 -0400

    really support multiple domains

--- src/legacy/adapter.js
+++ src/legacy/adapter.js
@@ -1,26 +1,29 @@
 import ShopifyBuy from '../shopify-buy-ui';
+import UI from '../ui';
 import EmbedWrapper from './embed-wrapper';
 
 // Use the Buy Button Channel API key
 const apiKey = window.SHOPIFY_BUY_UI_ADAPTER_API_KEY || '395ba487a5981e6e573b5ab104645271';
 const appId = window.SHOPIFY_BUY_UI_ADAPTER_APP_ID || 6;
 
-export class Adapter {
+class Adapter {
   constructor() {
     this.uis = {};
   }
   init() {
-    this.elements = [...document.querySelectorAll('[data-embed_type]')].map((element) => {
+    this.elements = [...document.querySelectorAll('[data-embed_type]')].map((element, index) => {
       const elem = new EmbedWrapper(element);
-      elem.render(this.getShopUI(elem.shop));
+      elem.render(this.getShopUI(elem.shop)).catch((error) => {
+        element.innerHTML = `Buy Button ${error}`;
+      });
       return elem;
     });
   }
-  getShopUI(shop) {
-    if (!this.uis[shop]) {
-      this.uis[shop] = ShopifyBuy.UI.init(ShopifyBuy.buildClient({apiKey, appId, domain: shop}));
+  getShopUI(domain) {
+    if (!this.uis[domain]) {
+      this.uis[domain] = new UI(ShopifyBuy.buildClient({apiKey, appId, domain}));
     }
-    return this.uis[shop];
+    return this.uis[domain];
   }
 }
 
