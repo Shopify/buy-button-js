b69507ca529824735a7c7eb877392d2d65630c6b
Tim Anema <tim.anema@shopify.com>
2016-08-17 11:30:07 -0400

    tests and linting

--- src/legacy/adapter.js
+++ src/legacy/adapter.js
@@ -6,12 +6,12 @@ import EmbedWrapper from './embed-wrapper';
 const apiKey = window.SHOPIFY_BUY_UI_ADAPTER_API_KEY || '395ba487a5981e6e573b5ab104645271';
 const appId = window.SHOPIFY_BUY_UI_ADAPTER_APP_ID || 6;
 
-class Adapter {
+export class Adapter {
   constructor() {
     this.uis = {};
   }
   init() {
-    this.elements = [...document.querySelectorAll('[data-embed_type]')].map((element, index) => {
+    this.elements = [...document.querySelectorAll('[data-embed_type]')].map((element) => {
       const elem = new EmbedWrapper(element);
       elem.render(this.getShopUI(elem.shop)).catch((error) => {
         element.innerHTML = `Buy Button ${error}`;
