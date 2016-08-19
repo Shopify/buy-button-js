5126af40e1539d7b0a9299ee2cd9ad9e588748d5
Tessa Thornton <tessathornton@gmail.com>
2016-08-19 13:11:47 -0400

    Legacy modal (#67)
    * html
    
    * move modal styles
    
    * un-rearrange dom
    
    * update modal styles
    
    * update tests
    
    * allow whitelist of styles to be passed from product to product modal
    
    * replace object.assign with merge
    
    * more styles
    
    * legacy modal styles
    
    * update dom for legacy modal
    
    * restyle legacy modal
    
    * fix styles on default modal
    
    * lints
    
    * fix test

--- src/components/product.js
+++ src/components/product.js
@@ -42,6 +42,44 @@ function whitelistedProperties(selectorStyles) {
   }, {});
 }
 
+function isPseudoSelector(key) {
+  return key.charAt(0) === ':';
+}
+
+function isMedia(key) {
+  return key.charAt(0) === '@';
+}
+
+const propertiesWhitelist = [
+  'background',
+  'background-color',
+  'border',
+  'border-radius',
+  'color',
+  'border-color',
+  'border-width',
+  'border-style',
+  'transition',
+  'text-transform',
+  'text-shadow',
+  'box-shadow',
+  'font-size',
+  'font-family',
+];
+
+function whitelistedProperties(selectorStyles) {
+  return Object.keys(selectorStyles).reduce((filteredStyles, propertyName) => {
+    if (isPseudoSelector(propertyName) || isMedia(propertyName)) {
+      filteredStyles[propertyName] = whitelistedProperties(selectorStyles[propertyName]);
+      return filteredStyles;
+    }
+    if (propertiesWhitelist.indexOf(propertyName) > -1) {
+      filteredStyles[propertyName] = selectorStyles[propertyName];
+    }
+    return filteredStyles;
+  }, {});
+}
+
 export default class Product extends Component {
   constructor(config, props) {
     super(config, props);
