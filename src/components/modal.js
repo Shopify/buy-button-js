6934e68521721b01f936c2eacf92b7e70909a01a
Tessa Thornton <tessathornton@gmail.com>
2016-08-24 18:03:14 -0400

    remove modal complexity

--- src/components/modal.js
+++ src/components/modal.js
@@ -29,33 +29,6 @@ export default class Modal extends Component {
     };
   }
 
-  get productTemplate() {
-    return new Template(this.product.templates, Object.assign({}, this.product.config.modalProduct.contents, {
-      img: false,
-    }), this.product.config.modalProduct.order);
-  }
-
-  get productModalTemplates() {
-    return {
-      img: `<div class="${this.classes.modal.img}">${this.product.templates.img}</div>`,
-      contents: `<div class="${this.classes.modal.contents} {{#data.currentImage}}${this.classes.modal.contentsWithImg}{{/data.currentImage}}"><div class="${this.classes.modal.scrollContents}">${this.productTemplate.masterTemplate}</div></div>`,
-      footer: `<div class="${this.classes.modal.footer} {{#data.currentImage}}${this.classes.modal.footerWithImg}{{/data.currentImage}}">
-                ${this.product.templates.button}
-              </div>`,
-    };
-  }
-
-  get productModalContents() {
-    return {
-      img: true,
-      contents: true,
-    };
-  }
-
-  get productModalOrder() {
-    return ['img', 'contents', 'footer'];
-  }
-
   delegateEvents() {
     super.delegateEvents();
     this.document.body.addEventListener('click', this.closeOnBgClick.bind(this));
@@ -95,6 +68,6 @@ export default class Modal extends Component {
     this.iframe.addClass('is-block');
     addClassToElement('is-active', this.wrapper);
     this.product = new Product(this.productConfig, this.props);
-    this.product.template = new Template(this.productModalTemplates, this.productModalContents, this.productModalOrder);
+    return this.product.init(this.model).then(() => this.loadImgs());
   }
 }
