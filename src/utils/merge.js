9dff5cad5ee1a96de9fe08fe2c6807849e20a43c
Tessa Thornton <tessathornton@gmail.com>
2016-08-24 17:39:52 -0400

    be even less dumb

--- src/utils/merge.js
+++ src/utils/merge.js
@@ -5,13 +5,12 @@ function isObject(value) {
 function merge(target, ...sources) {
   sources.forEach((source) => {
     if (source) {
-      var shit = Object.keys(source).reduce((acc, key) => {
+      Object.keys(source).forEach((key) => {
         if (isObject(source[key])) {
           source[key] = merge(target[key] || {}, source[key]);
         }
         target[key] = source[key];
-      }, {});
-      return Object.assign(target, shit);
+      });
     }
   });
   return target;
