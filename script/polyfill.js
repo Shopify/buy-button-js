946bbc74901a9188573192fa495f6a8b4f3c10d5
Tessa Thornton <tessathornton@gmail.com>
2016-08-29 15:20:08 -0400

    Build adapter (#86)
    * simplify sass compilation
    
    * add build scripts for adapter

--- script/polyfill.js
+++ script/polyfill.js
@@ -1,8 +1,9 @@
 var fs = require('fs');
 var resolve = require('resolve');
+var globalsFile = process.argv[2];
 
-var outPath = 'build/shopify-buy-ui.globals.polyfilled.js';
+var outPath = `build/${globalsFile}.globals.polyfilled.js`;
 var polyfills = fs.readFileSync(resolve.sync('shopify-buy/dist/polyfills.js'));
-var lib = fs.readFileSync('build/shopify-buy-ui.globals.js');
+var lib = fs.readFileSync(`build/${globalsFile}.globals.js`);
 
 fs.writeFileSync(outPath, polyfills + '\n' + lib);
