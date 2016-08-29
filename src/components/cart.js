976214235d7e0a6ace532db6538d6eb40e2720ff
Tessa Thornton <tessathornton@gmail.com>
2016-08-29 11:39:12 -0400

    lints

--- src/components/cart.js
+++ src/components/cart.js
@@ -125,17 +125,15 @@ export default class Cart extends Component {
   animateRemoveItem(id) {
     const el = this.document.getElementById(id);
     addClassToElement('is-hidden', el);
-    if (!el.parentNode) {
-      return;
-    }
     if (this.props.browserFeatures.transition) {
       el.addEventListener('transitionend', () => {
         // eslint-disable-next-line
         // TODO: why is transitionend sometimes called twice?
-        if (el.parentNode) {
-          el.parentNode.removeChild(el);
-          this.render();
+        if (!el.parentNode) {
+          return;
         }
+        el.parentNode.removeChild(el);
+        this.render();
       });
     } else {
       el.parentNode.removeChild(el);
