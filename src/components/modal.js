d97f20c0df3adba30a5e11c45b430cc3b32fd76b
Tessa Thornton <tessathornton@gmail.com>
2016-08-31 13:56:46 -0400

    clean up template wrapping

--- src/components/modal.js
+++ src/components/modal.js
@@ -28,6 +28,10 @@ export default class Modal extends Component {
     };
   }
 
+  wrapTemplate(html) {
+    return html;
+  }
+
   delegateEvents() {
     super.delegateEvents();
     this.document.body.addEventListener('click', this.closeOnBgClick.bind(this));
