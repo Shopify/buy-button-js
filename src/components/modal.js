9d349cf04e7329a919f6986deeb77eb009e240dc
Tessa Thornton <tessathornton@gmail.com>
2016-09-04 11:45:18 -0400

    fix browser bugs

--- src/components/modal.js
+++ src/components/modal.js
@@ -28,10 +28,6 @@ export default class Modal extends Component {
     };
   }
 
-  wrapTemplate(html) {
-    return html;
-  }
-
   delegateEvents() {
     super.delegateEvents();
     this.document.body.addEventListener('click', this.closeOnBgClick.bind(this));
