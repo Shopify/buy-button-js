630f12f9b1a3d7d25f0991f05b60bbe8a237e411
Tessa Thornton <tessathornton@gmail.com>
2016-09-21 14:37:34 -0400

    manually configure babel

--- script/build.js
+++ script/build.js
@@ -20,6 +20,26 @@ rollup({
   plugins: [
     babel({
       exclude: ['node_modules/hogan.js/**', 'node_modules/morphdom/**'],
+      plugins: [
+        require("babel-plugin-transform-es2015-template-literals"),
+        require("babel-plugin-transform-es2015-literals"),
+        require("babel-plugin-transform-es2015-function-name"),
+        require("babel-plugin-transform-es2015-arrow-functions"),
+        require("babel-plugin-transform-es2015-block-scoped-functions"),
+        [require("babel-plugin-transform-es2015-classes"), {loose: true}],
+        require("babel-plugin-transform-es2015-object-super"),
+        require("babel-plugin-transform-es2015-shorthand-properties"),
+        require("babel-plugin-transform-es2015-duplicate-keys"),
+        require("babel-plugin-transform-es2015-computed-properties"),
+        require("babel-plugin-check-es2015-constants"),
+        require("babel-plugin-transform-es2015-spread"),
+        require("babel-plugin-transform-es2015-parameters"),
+        require("babel-plugin-transform-es2015-destructuring"),
+        require("babel-plugin-transform-es2015-block-scoping"),
+        require("babel-plugin-transform-es2015-typeof-symbol"),
+        require("babel-plugin-transform-object-assign"),
+        require("babel-plugin-external-helpers")
+      ]
     }),
     nodeResolve({
       extensions: ['.js'],
