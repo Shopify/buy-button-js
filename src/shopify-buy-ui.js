37b06aff0f07727e7cc3fd50be7548318744f210
Tessa Thornton <tessathornton@gmail.com>
2016-09-23 13:35:59 -0400

    debug

--- src/shopify-buy-ui.js
+++ src/shopify-buy-ui.js
@@ -1,25 +1,32 @@
-import ShopifyBuy from 'shopify-buy/dist/shopify-buy.umd.polyfilled';
-import UI from './ui';
-import productTemplates from './templates/product';
+// import ShopifyBuy from 'shopify-buy/dist/shopify-buy.umd.polyfilled';
+// import UI from './ui';
+// import productTemplates from './templates/product';
 
-ShopifyBuy.UI = {
-  ui: null,
+// ShopifyBuy.UI = {
+//   ui: null,
 
-  init(client, integrations = {}, styleOverrides) {
-    if (!this.ui) {
-      this.ui = new UI(client, integrations, styleOverrides);
-    }
-    return this.ui;
-  },
+//   init(client, integrations = {}, styleOverrides) {
+//     if (!this.ui) {
+//       this.ui = new UI(client, integrations, styleOverrides);
+//     }
+//     return this.ui;
+//   },
 
-  adapterHelpers: {
-    templates: {
-      product: productTemplates,
-    },
-  },
+//   adapterHelpers: {
+//     templates: {
+//       product: productTemplates,
+//     },
+//   },
 
-  UIConstructor: UI,
-};
+//   UIConstructor: UI,
+// };
 
-window.ShopifyBuy = ShopifyBuy;
-export default ShopifyBuy;
+// window.ShopifyBuy = ShopifyBuy;
+// export default ShopifyBuy;
+//
+console.log('hello');
+
+export default function () {
+
+console.log('exported');
+}
