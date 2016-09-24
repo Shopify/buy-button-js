355200c68c0ca6121dff879fbb6815b7b10032db
Tessa Thornton <tessathornton@gmail.com>
2016-09-23 20:00:51 -0400

    document product set

--- src/components/product-set.js
+++ src/components/product-set.js
@@ -9,7 +9,17 @@ function isArray(arg) {
   return Object.prototype.toString.call(arg) === '[object Array]';
 }
 
+/**
+ * Renders and fetches data for collection and product set embed.
+ * @extends Component.
+ */
+
 export default class ProductSet extends Component {
+  /**
+   * create ProductSet
+   * @param {Object} config - configuration object.
+   * @param {Object} props - data and utilities passed down from UI instance.
+   */
   constructor(config, props) {
     super(config, props);
     this.products = [];
@@ -22,6 +32,10 @@ export default class ProductSet extends Component {
     this.resizeCompleted = false;
   }
 
+  /**
+   * get key for configuration object.
+   * @return {String}
+   */
   get typeKey() {
     return 'productSet';
   }
@@ -34,6 +48,10 @@ export default class ProductSet extends Component {
     return true;
   }
 
+  /**
+   * get data to be passed to view.
+   * @return {Object} viewData object.
+   */
   get viewData() {
     return {
       classes: this.classes,
@@ -42,12 +60,20 @@ export default class ProductSet extends Component {
     };
   }
 
+  /**
+   * get events to be bound to DOM.
+   * @return {Object}
+   */
   get DOMEvents() {
     return Object.assign({}, {
       [`click .${this.classes.productSet.paginationButton.split(' ').join('.')}`]: this.nextPage.bind(this),
     }, this.options.DOMEvents);
   }
 
+  /**
+   * get template for rendering pagination button.
+   * @return {Object} Template instance
+   */
   get paginationTemplate() {
     this._paginationTemplate = this._paginationTemplate || new Template({pagination: this.options.templates.pagination}, {pagination: true}, ['pagination']);
     return this._paginationTemplate;
@@ -60,6 +86,13 @@ export default class ProductSet extends Component {
     };
   }
 
+  /**
+   * initializes component by creating model and rendering view.
+   * Creates and initalizes cart if necessary.
+   * Calls renderProducts.
+   * @param {Object} [data] - data to initialize model with.
+   * @return {Promise} promise resolving to instance.
+   */
   init(data) {
     return super.init.call(this, data).then((model) => (
       this.props.createCart({options: this.config}).then((cart) => {
@@ -72,6 +105,11 @@ export default class ProductSet extends Component {
     ));
   }
 
+  /**
+   * fetches products from SDK based on provided config information.
+   * @param {Object} options - query options for request
+   * @return {Promise} promise resolving to collection data.
+   */
   sdkFetch(options = {}) {
 
     /* eslint-disable camelcase */
@@ -96,6 +134,11 @@ export default class ProductSet extends Component {
     return `<div class="${this.classes.productSet.productSet}">${html}</div>`;
   }
 
+  /**
+   * call sdkFetch and set model.products to products array.
+   * @throw 'Not Found' if model not returned.
+   * @return {Promise} promise resolving to model data.
+   */
   fetchData() {
     return this.sdkFetch().then((products) => {
       if (products.length) {
@@ -107,6 +150,10 @@ export default class ProductSet extends Component {
     });
   }
 
+  /**
+   * make request to SDK for current page + 1 to determine if next page exists. Render button if next page exists.
+   * @return {Promise} promise resolving when button is rendered or not.
+   */
   showPagination() {
     return this.sdkFetch({page: this.page + 1}).then((data) => {
       this.nextModel = {products: data};
@@ -116,12 +163,18 @@ export default class ProductSet extends Component {
     });
   }
 
+  /**
+   * append next page worth of products into the DOM
+   */
   nextPage() {
     this.model = this.nextModel;
     this.page = this.page + 1;
     this.renderProducts();
   }
 
+  /**
+   * resize iframe until it is tall enough to contain all products.
+   */
   resizeUntilFits() {
     if (!this.iframe || this.resizeCompleted) {
       return;
@@ -145,12 +198,22 @@ export default class ProductSet extends Component {
     }, pollInterval);
   }
 
+  /**
+   * re-assign configuration and re-render component.
+   * Update Cart component if necessary.
+   * Call renderProducts.
+   * @param {Object} config - new configuration object.
+   */
   updateConfig(config) {
     super.updateConfig(config);
     this.cart.updateConfig(config);
     this.renderProducts();
   }
 
+  /**
+   * render product components into productSet container. Show pagination button if necessary.
+   * @return {Promise} promise resolving to instance.
+   */
   renderProducts() {
     if (!this.model.products.length) {
       return Promise.resolve();
