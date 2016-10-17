3e80523728c59acc625867ab2dd5558afca060f3
Tessa Thornton <tessathornton@gmail.com>
2016-10-17 10:29:03 -0400

    namespace all class names

--- src/styles/embeds/conditional.js
+++ src/styles/embeds/conditional.js
@@ -1,1 +1,1 @@
-export default ".modal {\n  display: none; }\n  .is-active .modal {\n    display: block;\n    opacity: 1;\n    margin-left: auto;\n    margin-right: auto; }\n"
+export default ".shopify-buy__modal {\n  display: none; }\n  .is-active .shopify-buy__modal {\n    display: block;\n    opacity: 1;\n    margin-left: auto;\n    margin-right: auto; }\n"
