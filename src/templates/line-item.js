703dbc82c830ccdf39a2cc712fdfbb6e8eee10fe
Haris Mahmood <harismahmood89@gmail.com>
2016-10-20 13:18:34 -0400

    use svgs for quantity controls, and update heights and font sizing

--- src/templates/line-item.js
+++ src/templates/line-item.js
@@ -5,9 +5,9 @@ const lineItemTemplates = {
   title: '<span class="{{data.classes.lineItem.itemTitle}}">{{data.title}}</span>',
   price: '<span class="{{data.classes.lineItem.price}}">${{data.line_price}}</span>',
   quantity: `<div class="{{data.classes.lineItem.quantity}}">
-              <button class="{{data.classes.lineItem.quantityButton}} {{data.classes.lineItem.quantityDecrement}}" type="button" data-line-item-id="{{data.id}}"><span>-</span><span class="visuallyhidden">Decrement</span></button>
+              <button class="{{data.classes.lineItem.quantityButton}} {{data.classes.lineItem.quantityDecrement}}" type="button" data-line-item-id="{{data.id}}"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16"><path d="M4 7h8v2H4z"/></svg><span class="visuallyhidden">Decrement</span></button>
               <input class="{{data.classes.lineItem.quantityInput}}" type="number" min="0" aria-label="Quantity" data-line-item-id="{{data.id}}" value="{{data.quantity}}">
-              <button class="{{data.classes.lineItem.quantityButton}} {{data.classes.lineItem.quantityIncrement}}" type="button" data-line-item-id="{{data.id}}"><span>+</span><span class="visuallyhidden">Increment</span></button>
+              <button class="{{data.classes.lineItem.quantityButton}} {{data.classes.lineItem.quantityIncrement}}" type="button" data-line-item-id="{{data.id}}"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16"><path d="M12 7H9V4H7v3H4v2h3v3h2V9h3z"/></svg><span class="visuallyhidden">Increment</span></button>
             </div>`,
 };
 
