0fad2c144535a5823ba42bc5fdcc8ebe7cbdcb3e
Tessa Thornton <tessathornton@gmail.com>
2016-11-08 11:10:34 -0500

    add option to open checkout in same pane

--- src/defaults/components.js
+++ src/defaults/components.js
@@ -178,6 +178,7 @@ const defaults = {
     iframe: true,
     templates: cartTemplates,
     startOpen: false,
+    popup: true,
     manifest: ['cart', 'lineItem', 'toggle'],
     contents: {
       title: true,
