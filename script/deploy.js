ede29734185a9d9760487abeae7f45b6725ef78a
Tessa Thornton <tessathornton@gmail.com>
2016-11-11 08:50:52 -0500

    use uploader package to upload to s3 and npm

--- script/deploy.js
+++ script/deploy.js
@@ -1,15 +1,12 @@
 "use strict";
 
+const Uploader = require('js-uploader');
 const path = require('path');
 const fs = require('fs');
 const mime = require('mime-types');
 const currentVersion = require('../package.json').version;
-const s3Dir = 'buy-button';
 const awsConfig = require('../config.json').aws;
 const awsSDK = require('aws-sdk');
-const distDir = 'dist';
-const filePaths = fs.readdirSync(distDir);
-const defaultContentType = 'application/javascript';
 
 const awsS3 = new awsSDK.S3({
   accessKeyId: awsConfig.accessKeyId,
@@ -21,29 +18,12 @@ const awsS3 = new awsSDK.S3({
   }
 });
 
-const uploadFile = function(filePath, s3Key) {
-  awsS3.putObject({
-    Body: fs.readFileSync(distDir + '/' + filePath),
-    Key: s3Key,
-    ContentType: getContentType(distDir + '/' + filePath),
-  }, function(error) {
-    if(error) {
-      return console.error('Error uploading ' + filePath, error);
-    }
-
-    console.info('Uploaded "' + filePath + '" to S3 as "' + s3Key + '"');
-  });
-}
-
-const getContentType = function(filePath) {
-  return mime.lookup(filePath) || defaultContentType;
-}
-
-filePaths.map(function(filePath) {
-  const basename = path.basename(filePath);
-  const latestName = s3Dir + '/latest/' + basename;
-  const versionedName = s3Dir + '/' + currentVersion + '/' + basename;
-
-  uploadFile(filePath, latestName);
-  uploadFile(filePath, versionedName);
+const uploader = new Uploader({
+  s3: awsS3,
+  dir: 'dist',
+  destination: 'buy-button',
+  version: currentVersion,
 });
+
+uploader.deployStaticFiles();
+uploader.npmPublish();
