09c571c59b7aa4081ecc5a547b8e531ec89902ad
Haris Mahmood <harismahmood89@gmail.com>
2017-01-09 09:55:19 -0500

    big update to postcss and cssnext for style transforms

--- script/embed-styles.js
+++ script/embed-styles.js
@@ -1,35 +1,40 @@
-var sass = require('node-sass');
 var fs = require('fs');
+var postcss = require('postcss')
+var cssnext = require('postcss-cssnext')
+var cssimports = require('postcss-import')
 
 var embedsCSS = 'const styles = {};';
 
-embedsCSS += fs.readdirSync('src/styles/embeds/sass/manifests').reduce(function (acc, file) {
+var promises = fs.readdirSync('src/styles/embeds/sass/manifests').map(function(file) {
   var fileRoot = file.split('.')[0];
-  var result = sass.renderSync({file: './src/styles/embeds/sass/manifests/' + file, outputStyle: 'compressed'});
-  return acc + '\n styles.' + fileRoot + ' = \'' + result.css.toString().replace(/(\r\n|\n|\r)/gm," ") + '\'';
-}, '');
 
-embedsCSS += '\n export default styles';
-
-fs.writeFileSync('src/styles/embeds/all.js', embedsCSS);
-
-sass.render({
-  file: './src/styles/embeds/sass/conditional.scss'
-}, function (err, css) {
-  if (err) {
-    console.error(err);
-  } else {
-    var js = css.css.toString();
-    fs.writeFileSync('src/styles/embeds/conditional.js', 'export default ' + JSON.stringify(js));
-  }
+  return postcss([cssimports, cssnext])
+    .process(fs.readFileSync('./src/styles/embeds/sass/manifests/' + file), {from: './src/styles/embeds/sass/manifests/' + file})
+    .then(function(result) {
+      return {file: fileRoot, css: result.css};
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
 });
 
-sass.render({
-  file: './src/styles/manifest.scss'
-}, function (err, css) {
-  if (err) {
-    console.error(err);
-  } else {
-    fs.writeFileSync('./dist/buybutton.css', css.css);
-  }
+Promise.all(promises).then(function(values) {
+  embedsCSS += values.reduce(function(acc, value) {
+    return acc + '\n styles.' + value.file + ' = \'' + value.css.replace(/(\r\n|\n|\r)/gm, ' ') + '\'';
+  }, '');
+
+  embedsCSS += '\n export default styles';
+  fs.writeFileSync('src/styles/embeds/all.js', embedsCSS);
+})
+.catch(function(err) {
+  console.log(err);
 });
+
+postcss([cssnext])
+  .process(fs.readFileSync('./src/styles/embeds/sass/conditional.css'))
+  .then(function(result) {
+    fs.writeFileSync('src/styles/embeds/conditional.js', 'export default ' + JSON.stringify(result.css));
+  })
+  .catch(function(err) {
+    console.log(err);
+  });
