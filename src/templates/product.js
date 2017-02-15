339f1bf165a797d6490572402b12fed5c6c32e36
Tessa Thornton <tessathornton@gmail.com>
2017-02-15 08:44:33 -0500

    add data-element to templates (#346)
    * add data-element to templates
    
    * resolve conflict

--- src/templates/product.js
+++ src/templates/product.js
@@ -1,43 +1,43 @@
-const quantityTemplate = `<div class="{{data.classes.product.quantity}} {{data.quantityClass}}">
+const quantityTemplate = `<div class="{{data.classes.product.quantity}} {{data.quantityClass}}" data-element="product.quantity">
             {{#data.contents.quantityDecrement}}
-              <button class="{{data.classes.product.quantityButton}} {{data.classes.product.quantityDecrement}}" type="button"><span>-</span><span class="visuallyhidden">Decrement</span></button>
+              <button class="{{data.classes.product.quantityButton}} {{data.classes.product.quantityDecrement}}" type="button" data-element="product.quantityDecrement"><span>-</span><span class="visuallyhidden">Decrement</span></button>
             {{/data.contents.quantityDecrement}}
             {{#data.contents.quantityInput}}
-              <input class="{{data.classes.product.quantityInput}}" type="number" min="0" aria-label="Quantity" value="{{data.selectedQuantity}}">
+              <input class="{{data.classes.product.quantityInput}}" type="number" min="0" aria-label="Quantity" value="{{data.selectedQuantity}}" data-element="product.quantityInput">
             {{/data.contents.quantityInput}}
             {{#data.contents.quantityIncrement}}
-              <button class="{{data.classes.product.quantityButton}} {{data.classes.product.quantityIncrement}}" type="button"><span>+</span><span class="visuallyhidden">Increment</span></button>
+              <button class="{{data.classes.product.quantityButton}} {{data.classes.product.quantityIncrement}}" type="button" data-element="product.quantityIncrement"><span>+</span><span class="visuallyhidden">Increment</span></button>
             {{/data.contents.quantityIncrement}}
            </div>`;
-const buttonTemplate = '<div class="{{data.classes.product.buttonWrapper}}"><button {{#data.buttonDisabled}}disabled{{/data.buttonDisabled}} class="{{data.classes.product.button}} {{data.buttonClass}}">{{data.buttonText}}</button></div>';
+const buttonTemplate = '<div class="{{data.classes.product.buttonWrapper}}" data-element="product.buttonWrapper"><button {{#data.buttonDisabled}}disabled{{/data.buttonDisabled}} class="{{data.classes.product.button}} {{data.buttonClass}}" data-element="product.button">{{data.buttonText}}</button></div>';
 
 const productTemplate = {
-  img: '<div class="{{data.classes.product.imgWrapper}}"><img class="{{data.classes.product.img}}" src="{{data.currentImage.src}}" /></div>',
-  imgWithCarousel: `<div class="{{data.classes.product.imgWrapper}}">
+  img: '<div class="{{data.classes.product.imgWrapper}}" data-element="product.imgWrapper"><img data-element="product.img" class="{{data.classes.product.img}}" src="{{data.currentImage.src}}" /></div>',
+  imgWithCarousel: `<div class="{{data.classes.product.imgWrapper}}" data-element="product.imageWrapper">
                       <div class="main-image-wrapper">
                         <button type="button" class="carousel-button carousel-button--previous">Left</button>
-                        <img class="{{data.classes.product.img}}" src="{{data.currentImage.src}}" />
+                        <img class="{{data.classes.product.img}}" src="{{data.currentImage.src}}" data-element="product.img" />
                         <button type="button" class="carousel-button carousel-button--next">Right</button>
                       </div>
                       <div class="{{data.classes.product.carousel}}">
                         {{#data.carouselImages}}
-                        <a href="{{src}}" class="{{data.classes.product.carouselItem}} {{#isSelected}} {{data.classes.product.carouselItemSelected}} {{/isSelected}}" data-image-id="{{id}}" style="background-image: url({{carouselSrc}})"></a>
+                        <a data-element="product.carouselitem" href="{{src}}" class="{{data.classes.product.carouselItem}} {{#isSelected}} {{data.classes.product.carouselItemSelected}} {{/isSelected}}" data-image-id="{{id}}" style="background-image: url({{carouselSrc}})"></a>
                         {{/data.carouselImages}}
                       </div>
                     </div>`,
-  title: '<h1 class="{{data.classes.product.title}}">{{data.title}}</h1>',
-  variantTitle: '{{#data.hasVariants}}<h2 class="{{data.classes.product.variantTitle}}">{{data.selectedVariant.title}}</h2>{{/data.hasVariants}}',
-  options: '{{#data.hasVariants}}<div class="{{data.classes.product.options}}">{{{data.optionsHtml}}}</div>{{/data.hasVariants}}',
-  price: `<div class="{{data.classes.product.prices}}">
+  title: '<h1 class="{{data.classes.product.title}}" data-element="product.title">{{data.title}}</h1>',
+  variantTitle: '{{#data.hasVariants}}<h2 class="{{data.classes.product.variantTitle}}" data-element="product.variantTitle">{{data.selectedVariant.title}}</h2>{{/data.hasVariants}}',
+  options: '{{#data.hasVariants}}<div class="{{data.classes.product.options}}" data-element="product.options">{{{data.optionsHtml}}}</div>{{/data.hasVariants}}',
+  price: `<div class="{{data.classes.product.prices}}" data-element="product.prices">
             {{#data.selectedVariant}}
-            <span class="{{data.classes.product.price}} {{data.priceClass}}">{{data.formattedPrice}}</span>
-            {{#data.selectedVariant.compareAtPrice}}<span class="{{data.classes.product.compareAt}}">{{data.formattedCompareAtPrice}}</span>{{/data.selectedVariant.compareAtPrice}}
+            <span class="{{data.classes.product.price}} {{data.priceClass}}" data-element="product.price">{{data.formattedPrice}}</span>
+            {{#data.selectedVariant.compareAtPrice}}<span class="{{data.classes.product.compareAt}}" data-element="product.compareAt">{{data.formattedCompareAtPrice}}</span>{{/data.selectedVariant.compareAtPrice}}
             {{/data.selectedVariant}}
           </div>`,
-  description: '<div class="{{data.classes.product.description}}">{{{data.description}}}</div>',
+  description: '<div class="{{data.classes.product.description}}" data-element="product.description">{{{data.description}}}</div>',
   button: buttonTemplate,
   quantity: quantityTemplate,
-  buttonWithQuantity: `<div class="{{data.classes.product.buttonWithQuantity}}">${quantityTemplate}${buttonTemplate}</div>`,
+  buttonWithQuantity: `<div class="{{data.classes.product.buttonWithQuantity}}" data-element="product.buttonWithQuantity">${quantityTemplate}${buttonTemplate}</div>`,
 };
 
 export default productTemplate;
