5a389afbb0278f6b9c2f006b683e480c3cb2355e
Haris Mahmood <harismahmood89@gmail.com>
2017-05-08 10:31:01 -0400

    replatforming wip

--- src/buybutton.js
+++ src/buybutton.js
@@ -1,16 +1,18 @@
-import ShopifyBuy from 'shopify-buy/polyfilled';
+// import ShopifyBuy from 'shopify-buy/polyfilled';
+import ShopifyBuy, {Config} from 'shopify-buy';
 import UI from './ui';
 import productTemplates from './templates/product';
 
 window.ShopifyBuy = window.ShopifyBuy || ShopifyBuy;
+window.Config = window.Config || Config;
 
 ShopifyBuy.UI = window.ShopifyBuy.UI || {
   domains: {},
 
-  init(client, integrations = {}, styleOverrides) {
-    const domain = client.config.domain;
+  init(client, config, integrations = {}, styleOverrides) {
+    const domain = config.domain;
     if (!this.domains[domain]) {
-      this.domains[domain] = new UI(client, integrations, styleOverrides);
+      this.domains[domain] = new UI(client, config, integrations, styleOverrides);
     }
     return this.domains[domain];
   },
