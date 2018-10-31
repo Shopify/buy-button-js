0e4f56fa61b266606c08c8565a4c2cdf26d631c1
James Kim <james.kim@shopify.com>
2018-10-31 10:58:15 -0400

    added unit tests for ui.js

--- test/unit/ui.js
+++ test/unit/ui.js
@@ -1,5 +1,6 @@
 import ShopifyBuy from '../../src/buybutton';
 import UI from '../../src/ui';
+import Tracker from '../../src/utils/track';
 import Product from '../../src/components/product';
 import Cart from '../../src/components/cart';
 import shopFixture from '../fixtures/shop-info';
@@ -41,6 +42,50 @@ describe('ui class', () => {
       assert.equal(ui.config.domain, client.config.domain);
       assert(shopInfo.notCalled);
     });
+
+    it('creates an empty components object holding product, cart, collection, productSet, modal, and toggle', () => {
+      const emptyComponentsObj = {
+        product: [],
+        cart: [],
+        collection: [],
+        productSet: [],
+        modal: [],
+        toggle: [],
+      };
+      ui = new UI(client);
+
+      assert.deepEqual(ui.components, emptyComponentsObj);
+    });
+
+    it('sets an error reporter from integrations props', () => {
+      const integrations = {errorReporter: 'test'};
+      ui = new UI(client, integrations);
+      assert.equal(ui.errorReporter, integrations.errorReporter);
+    });
+
+    it('creates a tracker', () => {
+      const integrations = {tracker: 'test'};
+      ui = new UI(client, integrations);
+      assert.instanceOf(ui.tracker, Tracker);
+    });
+
+    it('sets up event bindings', () => {
+      const resizeSpy = sinon.spy(UI.prototype, '_bindResize');
+      const hostClickSpy = sinon.spy(UI.prototype, '_bindHostClick');
+      const escSpy = sinon.spy(UI.prototype, '_bindEsc');
+      const postMessageSpy = sinon.spy(UI.prototype, '_bindPostMessage');
+      ui = new UI(client);
+
+      assert.calledOnce(resizeSpy);
+      assert.calledOnce(hostClickSpy);
+      assert.calledOnce(escSpy);
+      assert.calledOnce(postMessageSpy);
+
+      resizeSpy.restore();
+      hostClickSpy.restore();
+      escSpy.restore();
+      postMessageSpy.restore();
+    });
   });
 
   describe('createCart', () => {
@@ -61,7 +106,7 @@ describe('ui class', () => {
           assert.equal(1, ui.components.cart.length, 'cart array has 1 item');
           ui.destroyComponent('cart', ui.components.cart[0].model.id);
         });
-      })
+      });
     });
 
     describe('when a cart exists', () => {
@@ -87,22 +132,56 @@ describe('ui class', () => {
       initStub.restore();
     });
 
-    it('creates new component of type', () => {
+    it('creates new component of type with tracker attached', () => {
       return ui.createComponent('product', productConfig).then(() => {
         assert.equal(1, ui.components.product.length);
+        assert.calledOnce(initStub);
+        assert.calledOnce(trackStub);
         ui.destroyComponent('product', ui.components.product[0].model.id);
       });
     });
 
     it('passes config to constructor', () => {
       productConfig.node = null;
-      const testConfig = {
-        id: 123,
-        options: {},
-      }
       return ui.createComponent('product', productConfig).then(() => {
         assert.equal(null, ui.components.product[0].config.node);
         ui.destroyComponent('product', ui.components.product[0].model.id);
+      });
+    });
+
+    it('binds escape key on iframe if it exists', () => {
+      productConfig.iframe = true;
+      const escSpy = sinon.stub(UI.prototype, '_bindEsc');
+      ui = new UI(client);
+
+      return ui.createComponent('product', productConfig).then(() => {
+        assert.calledOnce(escSpy);
+        escSpy.restore();
+        ui.components.product = []; // parentNode does not exist on product.node so .destroyComponent does not work here
+      });
+    });
+
+    it('catches any error from component.init and notifies errorReporter', () => {
+      initStub.restore(); // removes the regular initStub to replace with the initStub that throws an error
+      const integrations = {errorReporter: {notifyException: sinon.spy()}};
+      const errorInitStub = sinon.stub(Product.prototype, 'init').returns(Promise.reject({errors: [{message: 'rejected.'}]}));
+      ui = new UI(client, integrations);
+
+      ui.createComponent('product', productConfig).then(() => {
+        assert.throws(ui.createComponent, Error);
+        assert.calledOnce(integrations.errorReporter.notifyException);
+        errorInitStub.restore();
+        ui.destroyComponent('product', ui.components.product[0].model.id);
+      });
+    });
+
+    it('grabs node from _queryEntryNode if no node is passed in from config', () => {
+      const queryEntryNodeSpy = sinon.spy(UI.prototype, '_queryEntryNode');
+      productConfig.node = null;
+
+      return ui.createComponent('product', productConfig).then(() => {
+        assert.calledOnce(queryEntryNodeSpy);
+        queryEntryNodeSpy.restore();
       });
     });
   });
@@ -111,14 +190,148 @@ describe('ui class', () => {
     it('removes component and calls its destroy method', () => {
       const testCart = {
         model: {
-          id: 123
+          id: 123,
         },
-        destroy: sinon.spy()
-      }
+        destroy: sinon.spy(),
+      };
       ui.components.cart.push(testCart);
       ui.destroyComponent('cart', 123);
       assert.equal(0, ui.components.cart.length);
       assert.calledOnce(testCart.destroy);
+    });
+  });
+
+  describe('trackComponent', () => {
+    let trackComponentStub;
+
+    beforeEach(() => {
+      trackComponentStub = sinon.stub(Tracker.prototype, 'trackComponent');
+    });
+
+    afterEach(() => {
+      trackComponentStub.restore();
+    });
+
+    it('if the component is productSet, track each product in set', () => {
+      const productSet = {
+        trackingInfo: [1, 2, 3, 4, 5],
+      };
+
+      ui.trackComponent('productSet', productSet);
+      assert.callCount(trackComponentStub, 5);
+    });
+
+    it('calls trackComponent with the given component type', () => {
+      const component = {
+        trackingInfo: 'test',
+      };
+
+      ui.trackComponent('product', component);
+      assert.calledWith(trackComponentStub, 'product');
+      ui.trackComponent('cart', component);
+      assert.calledWith(trackComponentStub, 'cart');
+      ui.trackComponent('modal', component);
+      assert.calledWith(trackComponentStub, 'modal');
+      ui.trackComponent('toggle', component);
+      assert.calledWith(trackComponentStub, 'toggle');
+    });
+  });
+
+  describe('cart methods', () => {
+    describe('closeCart', () => {
+      it('calls cart.close for every visible cart and calls restoreFocus after each close', () => {
+        const spy = sinon.spy();
+        const restoreFocusSpy = sinon.spy(UI.prototype, 'restoreFocus');
+        ui.components.cart = [
+          {isVisible: true, close: spy},
+          {isVisible: true, close: spy},
+          {isVisible: false, close: spy},
+        ];
+        ui.closeCart();
+        assert.callCount(spy, 2);
+        assert.callCount(restoreFocusSpy, 2);
+        restoreFocusSpy.restore();
+      });
+    });
+
+    describe('openCart', () => {
+      it('calls cart.open for every cart', () => {
+        const spy = sinon.spy();
+        ui.components.cart = [
+          {isVisible: true, open: spy},
+          {isVisible: true, open: spy},
+          {isVisible: false, open: spy},
+        ];
+        ui.openCart();
+        assert.callCount(spy, 3);
+      });
+    });
+
+    describe('toggleCart', () => {
+      it('calls cart.toggleVisilbity for every cart', () => {
+        const spy = sinon.spy();
+        ui.components.cart = [
+          {toggleVisibility: spy},
+          {toggleVisibility: spy},
+          {toggleVisibility: spy},
+        ];
+        ui.toggleCart(true);
+        assert.callCount(spy, 3);
+      });
+
+      it('calls restoreFocus if cart is not visible', () => {
+        const spy = sinon.spy();
+        const restoreFocusSpy = sinon.spy(UI.prototype, 'restoreFocus');
+        ui.components.cart = [
+          {toggleVisibility: spy},
+          {toggleVisibility: spy},
+          {toggleVisibility: spy},
+        ];
+        ui.toggleCart(true);
+        assert.notCalled(restoreFocusSpy);
+        ui.toggleCart(false);
+        assert.calledOnce(restoreFocusSpy);
+        restoreFocusSpy.restore();
+
+      });
+    });
+  });
+
+  describe('createModal', () => {
+    it('creates a new modal if one does not already exist', () => {
+      ui.createModal({options: {}});
+      assert.equal(1, ui.components.modal.length);
+      ui.destroyComponent('modal', ui.components.modal[0].model.id);
+    });
+
+    it('does not create a modal if it already exists', () => {
+      ui.createModal({options: {}});
+      ui.createModal({options: {}});
+      assert.equal(1, ui.components.modal.length);
+      ui.destroyComponent('modal', ui.components.modal[0].model.id);
+    });
+  });
+
+  describe('closeModal', () => {
+    it('calls modal.close for every modal then restoreFocus once all is done', () => {
+      const spy = sinon.spy();
+      const restoreFocusSpy = sinon.spy(UI.prototype, 'restoreFocus');
+      ui.components.modal = [
+        {close: spy},
+        {close: spy},
+        {close: spy},
+      ];
+      ui.closeModal();
+      assert.callCount(spy, 3);
+      assert.calledOnce(restoreFocusSpy);
+      restoreFocusSpy.restore();
+    });
+  });
+
+  describe('setActiveEl', () => {
+    it('sets active element to element passed in', () => {
+      ui.setActiveEl('test');
+      assert.equal(ui.activeEl, 'test');
     });
   });
 });
