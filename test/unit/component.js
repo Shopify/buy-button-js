e6e788c65ee9203e6d8c2687a65c63a9a63f3085
James Kim <james.kim@shopify.com>
2018-11-06 16:35:46 -0500

    added tests for a few getters and private methods, updated old tests in component.js

--- test/unit/component.js
+++ test/unit/component.js
@@ -2,44 +2,62 @@ import Component from '../../src/component';
 import View from '../../src/view';
 import Updater from '../../src/updater';
 import * as logNotFound from '../../src/utils/log-not-found';
-
+import * as logger from '../../src/utils/logger';
+import * as isFunction from '../../src/utils/is-function';
 
 describe('Component class', () => {
+  let component;
+
+  beforeEach(() => {
+    component = new Component({id: 1234});
+  });
+
+  afterEach(() => {
+    component = null;
+  });
 
   describe('constructor', () => {
-    it('stores id, node, and handle on instance', () => {
+    it('stores id, storeFrontId, node, and handle on instance', () => {
       const node = document.createElement('div');
-      const component = new Component({
+      component = new Component({
         id: 1234,
         handle: 'lol',
+        storefrontId: 111,
         node,
       });
 
       assert.equal(component.id, 1234);
       assert.equal(component.handle, 'lol');
+      assert.equal(component.storefrontId, 111);
       assert.equal(component.node, node);
     });
 
     it('sets globalConfig based on passed in config', () => {
-      const component = new Component({
+      const expectedObj = {
+        debug: 'debug',
+        cartNode: 'cartNode',
+        moneyFormat: '${{amount}}',
+        modalNode: 'modalNode',
+        toggles: 'toggles',
+      };
+      const config = {
         id: 1234,
-      });
-      assert.equal(component.globalConfig.moneyFormat, '${{amount}}');
+        debug: 'debug',
+        cartNode: 'cartNode',
+        modalNode: 'modalNode',
+        toggles: 'toggles',
+      };
+      component = new Component(config);
+      assert.deepEqual(component.globalConfig, expectedObj);
     });
 
     it('instantiates a view', () => {
-      const component = new Component({
-        id: 1234,
-      });
       assert.instanceOf(component.view, View);
     });
 
     it('instantiates an updater', () => {
-      const component = new Component({
-        id: 1234,
-      });
       assert.instanceOf(component.updater, Updater);
-    })
+    });
 
     it('merges configuration options with defaults', () => {
       const config = {
@@ -49,98 +67,165 @@ describe('Component class', () => {
           },
         },
       };
-      const component = new Component(config);
+      component = new Component(config);
       assert.equal(component.config.product.buttonDestination, 'modal', 'configuration options override defaults');
       assert.equal(component.config.cart.iframe, true, 'defaults are merged into configuration');
     });
-  });
 
-  describe('get name()', () => {
-    it('returns name based on handle or id', () => {
-      let component = new Component({id: 1234});
-      assert.equal(component.name, 'frame-undefined-1234', 'uses ID if ID is set');
-      component = new Component({handle: 'lol'});
-      assert.equal(component.name, 'frame-undefined-lol', 'uses handle if handle is set');
+    it('sets props from props passed in', () => {
+      const props = 'hello';
+      component = new Component({id: 1234}, props);
+      assert.equal(component.props, 'hello');
+    });
+
+    it('instantiates an empty model object', () => {
+      assert.deepEqual(component.model, {});
     });
   });
 
-  describe('get options()', () => {
-    it('returns options for component by typeKey', () => {
-      const component = new Component({id: 1234});
-      component.typeKey = 'product';
-      assert.equal(component.options.buttonDestination, 'cart');
+  describe('getters', () => {
+    describe('get name()', () => {
+      it('returns name based on handle or id', () => {
+        assert.equal(component.name, 'frame-undefined-1234', 'uses ID if ID is set');
+        component = new Component({handle: 'lol'});
+        assert.equal(component.name, 'frame-undefined-lol', 'uses handle if handle is set');
+      });
     });
-  });
 
-  describe('get styles()', () => {
-    it('returns styles for each component in manifest', () => {
-      const component = new Component({
-        id: 1234,
-        options: {
-          product: {
-            styles: {
-              button: {
-                color: 'red',
+    describe('get options()', () => {
+      it('returns options for component by typeKey', () => {
+        component.typeKey = 'product';
+        assert.deepEqual(component.options, component.config.product);
+        assert.equal(component.options.buttonDestination, 'cart');
+      });
+    });
+
+    describe('get DOMEvents()', () => {
+      it('returns options.DOMEvents if it exists', () => {
+        const config = {options: {product: {DOMEvents: 'test'}}};
+        component = new Component(config);
+        component.typeKey = 'product';
+        assert.equal(component.DOMEvents, 'test');
+      });
+
+      it('returns an empty object is options.DOMEvents does not exist', () => {
+        const config = {options: {product: {DOMEvents: null}}};
+        component = new Component(config);
+        component.typeKey = 'product';
+        assert.deepEqual(component.DOMEvents, {});
+      });
+    });
+
+    describe('get events()', () => {
+      it('returns options.events if it exists', () => {
+        const config = {options: {product: {events: 'test'}}};
+        component = new Component(config);
+        component.typeKey = 'product';
+        assert.equal(component.events, 'test');
+      });
+
+      it('returns an empty object is options.events does not exist', () => {
+        const config = {options: {product: {events: null}}};
+        component = new Component(config);
+        component.typeKey = 'product';
+        assert.deepEqual(component.events, {});
+      });
+    });
+
+    describe('get styles()', () => {
+      it('returns styles for each component in manifest', () => {
+        component = new Component({
+          id: 1234,
+          options: {
+            product: {
+              styles: {
+                button: {
+                  color: 'red',
+                },
               },
             },
           },
-        },
+        });
+        component.typeKey = 'product';
+        assert.deepEqual(component.styles, {product: {button: {color: 'red'}}});
       });
-      component.typeKey = 'product';
-      assert.deepEqual(component.styles, {product: {button: {color: 'red'}}});
     });
-  });
 
-
-  describe('get classes()', () => {
-    it('returns classes for each component in manifest', () => {
-      const component = new Component({id: 1234});
-      component.typeKey = 'product';
-      assert.equal(component.classes.product.product, 'shopify-buy__product');
-      assert.equal(component.classes.option.option, 'shopify-buy__option-select');
+    describe('get classes()', () => {
+      it('returns classes for each component in manifest', () => {
+        component.typeKey = 'product';
+        assert.equal(component.classes.product.product, 'shopify-buy__product');
+        assert.equal(component.classes.option.option, 'shopify-buy__option-select');
+      });
     });
-  });
 
-  describe('get selectors()', () => {
-    it('returns classes formatted as css selectors for each component in manifest', () => {
-      const component = new Component({id: 1234});
-      component.typeKey = 'product';
-      assert.equal(component.selectors.product.product, '.shopify-buy__product');
-      assert.equal(component.selectors.option.option, '.shopify-buy__option-select');
+    describe('get selectors()', () => {
+      it('returns classes formatted as css selectors for each component in manifest', () => {
+        component.typeKey = 'product';
+        assert.equal(component.selectors.product.product, '.shopify-buy__product');
+        assert.equal(component.selectors.option.option, '.shopify-buy__option-select');
+      });
     });
-  });
 
-  describe('get googleFonts()', () => {
-    it('returns google fonts for each component in manifest', () => {
-      const config = {
-        options: {
-          product: {
-            googleFonts: ['lol'],
+    describe('get googleFonts()', () => {
+      it('returns google fonts for each component in manifest', () => {
+        const config = {
+          options: {
+            product: {
+              googleFonts: ['lol'],
+            },
+            option: {
+              googleFonts: ['bar'],
+            },
           },
-          option: {
-            googleFonts: ['bar'],
-          },
-        },
-      };
-      const component = new Component(config);
-      component.typeKey = 'product';
-      assert.deepEqual(component.googleFonts, ['lol', 'bar']);
+        };
+        component = new Component(config);
+        component.typeKey = 'product';
+        assert.deepEqual(component.googleFonts, ['lol', 'bar']);
+      });
     });
-  });
 
-  describe('get viewData()', () => {
-    it('returns model and some other properties', () => {
-      const component = new Component({id: 1234});
-      component.typeKey = 'product';
-      component.model = {test: 'lol'};
-      assert.equal(component.viewData.test, 'lol');
-      assert.equal(component.viewData.classes.product.product, 'shopify-buy__product');
+    describe('get viewData()', () => {
+      it('returns model and some other properties', () => {
+        component.typeKey = 'product';
+        component.model = {test: 'lol'};
+        assert.equal(component.viewData.test, 'lol');
+        assert.equal(component.viewData.classes.product.product, 'shopify-buy__product');
+      });
+    });
+
+    describe('get morphCallbacks()', () => {
+      it('returns an object with the function onBeforeElUpdated', () => {
+        assert.instanceOf(component.morphCallbacks.onBeforeElUpdated, Function);
+      });
+
+      describe('onBeforeElUpdated()', () => {
+        it('returns false if fromEl\'s tagname is img and its source is toEl\'s data-src element', () => {
+          const fromEl = {tagName: 'IMG', src: 'data-src'};
+          const toEl = {
+            getAttribute(param) {
+              return param;
+            },
+          };
+          assert.equal(component.morphCallbacks.onBeforeElUpdated(fromEl, toEl), false);
+        });
+
+        it('returns true if fromEl\'s tagname is not img or its source is not toEl\'s data-src element', () => {
+          const fromEl = {tagName: 'not IMG', src: 'not data-src'};
+          const toEl = {
+            getAttribute(param) {
+              return param;
+            },
+          };
+          assert.equal(component.morphCallbacks.onBeforeElUpdated(fromEl, toEl), true);
+        });
+      });
     });
   });
 
   describe('init()', () => {
     it('assigns model and initializes view', () => {
-      const component = new Component({id: 1234}, {
+      component = new Component({id: 1234}, {
         browserFeatures: {},
       });
       component.view = {
@@ -162,7 +247,7 @@ describe('Component class', () => {
     it('catches any error from setupModel', () => {
       const errorSetupModelStub = sinon.stub(Component.prototype, 'setupModel').returns(Promise.reject({message: ['Not Found']}));
       const logNotFoundStub = sinon.stub(logNotFound, 'default');
-      const component = new Component({id: 1234}, {
+      component = new Component({id: 1234}, {
         browserFeatures: {},
       });
       component.view = {
@@ -171,7 +256,7 @@ describe('Component class', () => {
         delegateEvents: sinon.spy(),
       };
       component.typeKey = 'product';
-      
+
       return component.init({lol: 'yes'}).catch(() => {
         assert.throws(component.init, Error);
         assert.calledOnce(logNotFoundStub);
@@ -182,14 +267,12 @@ describe('Component class', () => {
 
   describe('setupModel()', () => {
     it('returns passed data', () => {
-      const component = new Component({id: 1234});
       return component.setupModel({test: 'lol'}).then((model) => {
         assert.deepEqual(model, {test: 'lol'});
       });
     });
 
     it('calls fetchData if not passed data', () => {
-      const component = new Component({id: 1234});
       component.fetchData = sinon.stub().returns(Promise.resolve({test: 'lol'}));
       return component.setupModel().then((model) => {
         assert.deepEqual(model, {test: 'lol'});
@@ -199,7 +282,6 @@ describe('Component class', () => {
 
   describe('updateConfig()', () => {
     it('delegates to updater', () => {
-      const component = new Component({id: 1234});
       component.updater.updateConfig = sinon.spy();
       component.updateConfig({test: 'lol'});
       assert.calledWith(component.updater.updateConfig, {test: 'lol'});
@@ -208,10 +290,42 @@ describe('Component class', () => {
 
   describe('destroy()', () => {
     it('delegates to view', () => {
-      const component = new Component({id: 1234});
       component.view.destroy = sinon.spy();
       component.destroy();
       assert.calledOnce(component.view.destroy);
+    });
+  });
+
+  describe('_userEvent()', () => {
+    it('logs to logger if debug is set to true', () => {
+      const infoSpy = sinon.spy();
+      const loggerStub = sinon.stub(logger, 'default').value({info: infoSpy});
+      component.globalConfig.debug = true;
+      component.typeKey = 'key';
+      component._userEvent('test');
+      assert.calledWith(infoSpy, 'EVENT: test (key)');
+      loggerStub.restore();
+    });
+
+    it('does not log if debug is set to false', () => {
+      const infoSpy = sinon.spy();
+      const loggerStub = sinon.stub(logger, 'default').value({info: infoSpy});
+      component.globalConfig.debug = false;
+      component._userEvent('test');
+      assert.notCalled(infoSpy);
+      loggerStub.restore();
+    });
+
+    it('calls event if the method passed is a function in the event', () => {
+      const eventSpy = sinon.spy();
+      const config = {options: {product: {events: {test: eventSpy}}}};
+      component = new Component(config);
+      component.typeKey = 'product';
+
+      const isFunctionStub = sinon.stub(isFunction, 'default').returns(true);
+      component._userEvent('test');
+      assert.calledWith(eventSpy, component);
+      isFunctionStub.restore();
     });
   });
 });
