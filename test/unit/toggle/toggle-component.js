3846858e54520aacbe270de0b96b777cc8909c41
James Kim <james.kim@shopify.com>
2018-12-19 14:53:34 -0500

    removed appending and removal of cart node in tests except for the single test in constructor that tests for it

--- test/unit/toggle/toggle-component.js
+++ test/unit/toggle/toggle-component.js
@@ -3,7 +3,6 @@ import ToggleView from '../../../src/views/toggle';
 
 describe('Toggle Component class', () => {
   let toggle;
-  let cart;
   let node;
 
   beforeEach(() => {
@@ -11,28 +10,21 @@ describe('Toggle Component class', () => {
   });
 
   describe('constructor', () => {
-    beforeEach(() => {
-      cart = {
-        node: document.createElement('div'),
-      };
-      document.body.appendChild(cart.node);
-    });
-
-    afterEach(() => {
-      document.body.removeChild(cart.node);
-    });
-
     it('sets typeKey to toggle', () => {
-      toggle = new Toggle({node}, {cart});
+      toggle = new Toggle({node}, {});
       assert.equal(toggle.typeKey, 'toggle');
     });
 
     it('sets node to node in config if provided', () => {
-      toggle = new Toggle({node}, {cart});
+      toggle = new Toggle({node}, {});
       assert.equal(toggle.node, node);
     });
 
     it('sets node to a div element before cart node if config does not provide node', () => {
+      const cart = {
+        node: document.createElement('div'),
+      };
+      document.body.appendChild(cart.node);
       const div = document.createElement('div');
       const insertBeforeStub = sinon.stub(Node.prototype, 'insertBefore').returns(node);
       const createElementStub = sinon.stub(document, 'createElement').returns(div);
@@ -44,16 +36,18 @@ describe('Toggle Component class', () => {
 
       insertBeforeStub.restore();
       createElementStub.restore();
+      document.body.removeChild(cart.node);
     });
 
     it('creates a new toggle view', () => {
-      toggle = new Toggle({node}, {cart});
+      toggle = new Toggle({node}, {});
       assert.instanceOf(toggle.view, ToggleView);
     });
   });
 
   describe('prototype methods', () => {
     let toggleVisibilitySpy;
+    let cart;
 
     beforeEach(() => {
       toggleVisibilitySpy = sinon.spy();
@@ -73,12 +67,7 @@ describe('Toggle Component class', () => {
           ],
         },
       };
-      document.body.appendChild(cart.node);
       toggle = new Toggle({node}, {cart});
-    });
-
-    afterEach(() => {
-      document.body.removeChild(cart.node);
     });
 
     describe('toggleCart()', () => {
