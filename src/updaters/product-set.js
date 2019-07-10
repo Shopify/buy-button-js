d590515721ebaa6d6ad8653d89935a727ce06028
Spencer Canner <spencer.canner@shopify.com>
2019-07-10 13:52:17 -0400

    update modal of first product in product set

--- src/updaters/product-set.js
+++ src/updaters/product-set.js
@@ -3,7 +3,13 @@ import Updater from '../updater';
 export default class ProductSetUpdater extends Updater {
   updateConfig(config) {
     super.updateConfig(config);
-    this.component.props.destroyComponent('modal');
+    if (this.component.products[0].modal) {
+      this.component.products[0].modal.updateConfig(Object.assign({}, config, {
+        options: Object.assign({}, config.options, {
+          product: config.options.modalProduct,
+        }),
+      }));
+    }
     this.component.cart.updateConfig(config);
     this.component.renderProducts();
   }
