7bfe74368f44d701b4e5a70e54ff1cac9a6685cf
Spencer Canner <spencer.canner@shopify.com>
2019-07-18 11:45:38 -0400

    update product instead of modal for product set updates

--- src/updaters/product-set.js
+++ src/updaters/product-set.js
@@ -3,13 +3,9 @@ import Updater from '../updater';
 export default class ProductSetUpdater extends Updater {
   updateConfig(config) {
     super.updateConfig(config);
-    if (this.component.products[0].modal) {
-      this.component.products[0].modal.updateConfig(Object.assign({}, config, {
-        options: Object.assign({}, config.options, {
-          product: config.options.modalProduct,
-        }),
-      }));
-    }
+    this.component.products[0].updateConfig({
+      options: Object.assign({}, config.options),
+    });
     this.component.cart.updateConfig(config);
     this.component.renderProducts();
   }
